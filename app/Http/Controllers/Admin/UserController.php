1643f8aac94f52c88129da4fcbc3e04e8ce0ac99
jhonnstark <72222544+jhonnstark@users.noreply.github.com>
2020-10-03 15:48:58 -0500

    user creations

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Http\Resources\UserCollection;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Resources\User as UserResource;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
+    /**
+     * Display a listing view of the resource.
+     */
+    private $role = ['role' => 'user'];
+
+
     /**
      * Display a listing of the resource.
      *
@@ -17,7 +26,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('admin.list', ['role' => 'user']);
+        return view('admin.list', $this->role);
     }
 
     /**
@@ -33,22 +42,28 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function create()
     {
-        //
+        return view('admin.register', $this->role);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param UserRequest $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        $record = $request->validated();
+        $record['password'] = Hash::make($record['password']);
+        User::create($record);
+        return response()->json([
+            'status' => 201,
+            'message' => 'created',
+        ], 201);
     }
 
     /**
@@ -76,7 +91,7 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @param User $user
      * @return void
      */
