8e496b75cc3ea576778d71e720387c54844b7efd
Jonathan Cervantes <jonathan.cervantes@printerlogic.com>
2020-10-03 20:31:37 -0500

    teachers and admin delete

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Nombre de usuario o contraseña incorrecta.',
+    'throttle' => 'Has hecho demaciados intentos. Por favor espera :seconds segundos.',
 
 ];
